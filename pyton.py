cbb46a9343321304fd35997feef8b15950eb164a
slava <slava7479@mail.ru>
2022-11-25 20:12:46 +0300

    д\з 1

---
+++ pyton.py
@@ -0,0 +1,10 @@
+number = int(input('Введите числло для день недели от одного до семи')):
+if number < 1 or number > 7:
+    print ('Не верное значение,попробуйте ещё'):
+elif number >5:
+    print ("Выходной день!")
+elif number <=5:
+    print ('Рабочий день')
+    
+    
+        
